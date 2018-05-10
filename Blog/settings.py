d5228458f788ce8309b22f1dfc5c94e0d2478204
Melody <530631372@qq.com>
2018-05-10 09:49:39 +0800

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog',
         'USER': 'root',
-        'PASSWORD': 'sq416221@',
+        'PASSWORD': 'xxxxxxx',
         'HOST': 'localhost',
         'PORT': ''
     }
